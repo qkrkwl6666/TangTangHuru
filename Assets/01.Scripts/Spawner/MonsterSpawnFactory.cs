c82698facab276061f5be3759a58df703c88e4f6
박재홍 <qkrkwl6666@gmail.com>
2024-07-06 20:46:07 +0900

    [MonsterSpawnFactory] 몬스터 스폰 타입 기능 추가 1.몬스터 랜덤 스폰 2.몬스터 직선 스폰 (각도 조절 가능) 3.몬스터 원형 스폰 (몬스터 수 만큼 원형을 그려서 스폰)

--- Assets/01.Scripts/Spawner/MonsterSpawnFactory.cs
+++ Assets/01.Scripts/Spawner/MonsterSpawnFactory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -7,7 +8,9 @@ public class MonsterSpawnFactory : MonoBehaviour
 {
     private Transform playerTransform;
 
-    public float distance = 10f;
+    private float defaultDistance = 10f;
+    private float circleSpawnDistance = 10f;
+    private float lineSpawnDistance = 5f;
      
     private void Awake()
     {
@@ -18,11 +21,11 @@ public class MonsterSpawnFactory : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.F1))
         {
-            //CreateMonster(MonsterType.MonsterType1);
+            //CreateMonster(MonsterType.MonsterType1, 30, 3);
         }
         if (Input.GetKeyUp(KeyCode.F2))
         {
-            //CreateMonster(MonsterType.MonsterType2);
+            //CreateMonster(MonsterType.MonsterType2, 10, 2);
         }
         if (Input.GetKeyUp(KeyCode.F3))
         {
@@ -38,34 +41,65 @@ public class MonsterSpawnFactory : MonoBehaviour
             case 1:
                 for(int i = 0; i < spawnCount; i++)
                 {
-                    Addressables.InstantiateAsync(monsterType.ToString(), RandomPosition(), Quaternion.identity);
+                    Addressables.InstantiateAsync(monsterType.ToString(), RandomPosition(defaultDistance), Quaternion.identity);
                 }
                 break;
             // 送識 持失
             case 2:
+                var lineList = LinePosition(RandomPosition(lineSpawnDistance), spawnCount, 0f);
+
                 for (int i = 0; i < spawnCount; i++)
                 {
-                    //Addressables.InstantiateAsync(monsterType.ToString(), RandomPosition(), Quaternion.identity);
+                    Addressables.InstantiateAsync(monsterType.ToString(), lineList[i], Quaternion.identity);
                 }
                 break;
+            // 据 持失
             case 3:
                 for (int i = 0; i < spawnCount; i++)
                 {
-                    //Addressables.InstantiateAsync(monsterType.ToString(), RandomPosition(), Quaternion.identity);
+                    Addressables.InstantiateAsync(monsterType.ToString(), CirclePosition(spawnCount, i), Quaternion.identity);
                 }
                 break;
         }
     }
     //public void 
 
-    public Vector2 RandomPosition()
+    public Vector2 RandomPosition(float distance)
     {
         if (playerTransform == null) return Vector2.zero;
 
         Vector2 randomCirclePos = (Random.insideUnitCircle * distance);
-        Vector2 spawnPos = playerTransform.position * randomCirclePos;
+        Vector2 spawnPos = (Vector2)playerTransform.position + randomCirclePos;
 
         return spawnPos;
+    }
+
+    public Vector2 CirclePosition(int spawnCount, int currentSpawnCount)
+    {
+        if (playerTransform == null) return Vector2.zero;
+
+        float angle = ((360 / spawnCount) * currentSpawnCount) * Mathf.Deg2Rad;     
+
+        Vector2 CirclePos = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        Vector2 spawnPos = (Vector2)playerTransform.position + CirclePos * circleSpawnDistance;
+
+        return spawnPos;
+    }
+    public List<Vector2> LinePosition(Vector2 point, int spawnCount, float angle)
+    {
+        if (playerTransform == null) return null;
+
+        List<Vector2> lines = new List<Vector2>();
+
+        int padding = 3;
+
+        for(int i = 0; i < spawnCount; i++)
+        {
+            Vector2 pos = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * (padding * i);
+            lines.Add(point + pos);
+        }
+
+        return lines;
     }
 
 
