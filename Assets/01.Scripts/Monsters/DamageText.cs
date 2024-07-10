75c12a5e166cc50540a4070139a3b4c5c9c93c0c
정규성 <frostpine2214@gmail.com>
2024-07-10 13:41:04 +0900

    대미지 텍스트 수정중

--- Assets/01.Scripts/Monsters/DamageText.cs
+++ Assets/01.Scripts/Monsters/DamageText.cs
@@ -1,50 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class DamageText : MonoBehaviour
 {
-    public GameObject textObject;
-    private List<GameObject> textObjects = new();
-    private Vector3 textPos = new Vector3(0, 2, 0);
-
     private Monster monster;
 
     private void Awake()
     {
         monster = GetComponent<Monster>();
-        monster.OnDamaged += ShowDamage;
+        monster.OnDamaged += ShowHeadUpDamage;
     }
 
     private void OnDestroy()
     {
-        monster.OnDamaged -= ShowDamage;
+        monster.OnDamaged -= ShowHeadUpDamage;
     }
 
-    private void ShowDamage(float damage)
+    private void ShowHeadUpDamage(float damage)
     {
-        bool popped = false;
-        foreach (var text in textObjects)
-        {
-            if (!text.gameObject.activeSelf)
-            {
-                text.gameObject.SetActive(true);
-                text.gameObject.transform.position = transform.position + textPos;
-                text.GetComponent<TextMeshPro>().text = (damage / 1).ToString();
+        MonsterManager.Instance.ShowDamage(damage, transform.position);
 
-                popped = true;
-                break;
-            }
-        }
+        //if(textObjects.Count < 5)
+        //{
+        //    var newText = Instantiate(textObject, gameObject.transform.position, Quaternion.identity);
+        //    newText.GetComponent<TextMeshPro>().text = (damage / 1).ToString();
+        //    textObjects.Add(newText);
+        //    return;
+        //}
 
-        if (!popped)
-        {
-            var newText = Instantiate(textObject, gameObject.transform.position, Quaternion.identity);
-            newText.GetComponent<TextMeshPro>().text = (damage / 1).ToString();
-            textObjects.Add(newText);
-            popped = true;
-        }
+        //foreach (var text in textObjects)
+        //{
+        //    if (!text.gameObject.activeSelf)
+        //    {
+        //        text.gameObject.SetActive(true);
+        //        text.gameObject.transform.position = transform.position + textPos;
+        //        text.GetComponent<TextMeshPro>().text = (damage / 1).ToString();
+        //        return;
+        //    }
+        //}
     }
 }
--- Assets/01.Scripts/Weapons/WeaponComponents/Hit.cs
+++ Assets/01.Scripts/Weapons/WeaponComponents/Hit.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class Hit : MonoBehaviour
 {
-    public GameObject hitText;
-    public GameObject[] hitTexts;
-
     public LayerMask attackableLayer;
 
     public float damage;
@@ -17,8 +15,29 @@ public class Hit : MonoBehaviour
 
     private float totalDamage;
 
+    private bool attackable = true;
+    private float attackRate = 0.1f;
+    private float timer = 0f;
+
+    private void Update()
+    {
+        if(timer > attackRate)
+        {
+            attackable = true;
+            timer = 0f;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!attackable)
+            return;
+
         OnAttack(other);
     }
 
@@ -48,15 +67,8 @@ public class Hit : MonoBehaviour
                 gameObject.SetActive(false);
             }
 
+            attackable = false;
         }
-
-        //foreach(var hit in hitTexts)
-        //{
-        //    if (!hit.activeSelf)
-        //    {
-
-        //    }
-        //}
-        
     }
+
 }
