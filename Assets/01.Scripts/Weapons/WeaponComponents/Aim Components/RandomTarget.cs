50790932d57988500b882a0f31f9cb6462c8f8e0
정규성 <frostpine2214@gmail.com>
2024-07-18 15:39:47 +0900

    보조무기용 컴포넌트 작성중

--- Assets/01.Scripts/Weapons/WeaponComponents/Aim Components/RandomTarget.cs
+++ Assets/01.Scripts/Weapons/WeaponComponents/Aim Components/RandomTarget.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomTarget : MonoBehaviour
+public class RandomTarget : MonoBehaviour, IAimer
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public GameObject Player { get => player; }
+    public float LifeTime { get; set; }
+    public float Speed { get; set; }
+    public int TotalCount { get; set; }
+    public int Index { get; set; }
 
-    // Update is called once per frame
-    void Update()
+    private float range = 30f;
+    
+    public LayerMask targetLayer = LayerMask.GetMask("Enemy");
+
+    public Vector3 AimDirection()
     {
-        
+        var targets = Physics2D.CircleCastAll(transform.position, range, Vector2.zero, 0, targetLayer);
+
+        var index = Random.Range(0, targets.Length);
+
+        Vector3 result = targets[index].transform.position;
+
+        return result;
     }
 }
