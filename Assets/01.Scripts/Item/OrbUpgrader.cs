9029358035d58ce27952ca844aff3d2e0c6e8421
정규성 <frostpine2214@gmail.com>
2024-08-02 13:02:15 +0900

    제작탭 생성

--- Assets/01.Scripts/Item/OrbUpgrader.cs
+++ Assets/01.Scripts/Item/OrbUpgrader.cs
@@ -1,50 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OrbUpgrader : MonoBehaviour
 {
-    public Button[] upgradeButtons;
-    public GameObject popUp_OrbList;
+    public ItemSlotUI[] upgradeSlots;
+    public Button upgradeButton;
+    public OrbList popUp_OrbList;
+    public GameObject popUp_Notice;
 
     public Image prefab_Orb;
     public Image prefab_RareOrb;
     public Image prefab_EpicOrb;
 
-    public ItemSlotUI slotPrefab;
-    public ItemSlotUI upgradeSlot;
-
+    private int firstItemId;
 
     private void OnEnable()
     {
+        upgradeSlots = GetComponentsInChildren<ItemSlotUI>();
 
-
-        upgradeButtons = GetComponentsInChildren<Button>();
-
-        foreach(Button button in upgradeButtons )
+        foreach (var slot in upgradeSlots)
         {
-            button.onClick.AddListener(PopUpOrbList);
+            slot.GetComponent<Button>().onClick.AddListener(() => PopUpOrbList(slot));
         }
 
+        upgradeButton.onClick.AddListener(Upgrade);
     }
 
     private void OnDisable()
     {
     }
 
-    private void OnTransformChildrenChanged()
+    private void PopUpOrbList(ItemSlotUI slot)
     {
+        if(slot.currItemId != 0)
+        {
+            slot.currItemId = 0;
+            slot.slotIcon.gameObject.SetActive(false);
+        }
 
+        popUp_OrbList.gameObject.SetActive(true);
+        popUp_OrbList.currSlot = slot;
     }
 
-    private void UpgradeOrb()
+    private void Upgrade()
     {
+        if (!CheckUpgradable())
+            return;
 
+        GameManager.Instance.currSaveData.orb_Normal -= 3;
+        GameManager.Instance.currSaveData.orb_Rare++;
+        popUp_OrbList.ResetOn();
+
+        for (int i = 0; i < upgradeSlots.Length; i++)
+        {
+            upgradeSlots[i].slotIcon.gameObject.SetActive(false);
+        }
+        popUp_Notice.SetActive(true);
     }
 
-    private void PopUpOrbList()
+    private bool CheckUpgradable()
     {
-        popUp_OrbList.SetActive(true);
+        if (upgradeSlots == null || upgradeSlots.Length == 0)
+            return false;
+
+        firstItemId = upgradeSlots[0].currItemId;
+
+        for (int i = 1; i < upgradeSlots.Length; i++)
+        {
+            if (upgradeSlots[i].currItemId != firstItemId)
+            {
+                Debug.Log("배치된 오브가 모두 같아야 한다.");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
