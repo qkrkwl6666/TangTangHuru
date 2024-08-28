751742f043b8e1fa97c8ffdd44fd538945bf42c6
정규성 <frostpine2214@gmail.com>
2024-08-28 16:08:26 +0900

    오브 장착 가능. 인게임 수치반영 (세이브 정보없음)

--- Assets/01.Scripts/Inventory/WeaponOrbSlot.cs
+++ Assets/01.Scripts/Inventory/WeaponOrbSlot.cs
@@ -1,3 +1,5 @@
+using Spine;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,19 +9,23 @@ public class WeaponOrbSlot : MonoBehaviour
     public ItemSlotUI[] upgradeSlots;
     public OrbUpgrader orbUpgrader;
 
+    private bool resetOn = true;
 
     private void OnEnable()
     {
         var currWeapon = equipPopUp.currentItem as M_Weapon;
 
-        foreach (var slot in upgradeSlots)
+        if (resetOn)
         {
-            slot.GetComponent<Button>().onClick.AddListener(() => orbUpgrader.SlotSelected(slot));
+            foreach (var slot in upgradeSlots)
+            {
+                slot.GetComponent<Button>().onClick.AddListener(() => orbUpgrader.SlotSelected(slot));
+            }
         }
 
-        for(int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; i++)
         {
-            if(i < (int)currWeapon.ItemTier)
+            if (i < (int)currWeapon.ItemTier)
             {
                 upgradeSlots[i].gameObject.SetActive(true);
             }
@@ -28,6 +34,8 @@ public class WeaponOrbSlot : MonoBehaviour
                 upgradeSlots[i].gameObject.SetActive(false);
             }
         }
+
+        resetOn = false;
     }
 
     private void OnDisable()
@@ -39,13 +47,29 @@ public class WeaponOrbSlot : MonoBehaviour
             if(slot.currItemId == 0)
                 continue;
 
-            currWeapon.orbs.Add(equipPopUp.mainInventory.MainInventoryAddItem(slot.currItemId.ToString(), 0));
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddOrbToWeapon(ItemSlotUI slot)
     {
-        
+        var orbData = DataTableManager.Instance.Get<ItemTable>(DataTableManager.item).GetItemData(slot.currItemId.ToString());
+
+        var currWeapon = equipPopUp.currentItem as M_Weapon;
+        var selectedOrb = equipPopUp.mainInventory.GetItemTypesTier((ItemType)orbData.Item_Type, (ItemTier)orbData.Item_Tier);
+
+        if(currWeapon.orbs == null)
+        {
+            currWeapon.orbs = new();
+        }
+        currWeapon.orbs.Add(selectedOrb[0]);
+    }
+
+    public void RemoveOrbFromWeapon(ItemSlotUI slot)
+    {
+        var orbData = DataTableManager.Instance.Get<ItemTable>(DataTableManager.item).GetItemData(slot.currItemId.ToString());
+
+        var currWeapon = equipPopUp.currentItem as M_Weapon;
+        var selectedOrb = equipPopUp.mainInventory.GetItemTypesTier((ItemType)orbData.Item_Type, (ItemTier)orbData.Item_Tier);
+        currWeapon.orbs.Remove(selectedOrb[0]);
     }
 }
